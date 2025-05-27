754de1276696759bdac39360ea1328078576ade8
krishsinghhura <krishsinghhura@gmail.com>
2025-05-27 10:57:18 +0530

    changed middleware

--- Bonafide-Backend/middleware/univMiddleware.js
+++ Bonafide-Backend/middleware/univMiddleware.js
@@ -1,21 +1,29 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const univMiddleware = (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
-    console.log("token is",token);
-    
+  const authHeader = req.headers.authorization;
 
-    if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
-    }
+console.log(authHeader);
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;  // Add user info to req object
-        next();  // Proceed to next middleware or route handler
-    } catch (error) {
-        return res.status(401).json({ message: 'Invalid or expired token' });
-    }
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  console.log("token is", token);
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Add user info to req object
+    next(); // Proceed to next middleware or route handler
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
 };
 
 module.exports = univMiddleware;
