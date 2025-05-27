0fe3150edb4c06806a9991caf6fcb83280eb6ddd
krishsinghhura <krishsinghhura@gmail.com>
2025-05-27 12:01:58 +0530

    c

--- Bonafide-Backend/middleware/univMiddleware.js
+++ Bonafide-Backend/middleware/univMiddleware.js
@@ -3,17 +3,16 @@ const jwt = require("jsonwebtoken");
 const univMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-console.log(authHeader);
-
-  const token = authHeader.split(" ")[1];
-  console.log("token is", token);
-
 
   try {
     const decoded = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MzNmOTZkNDFhOTNlNjNlMDY1MGIwMSIsImlhdCI6MTc0ODI3OTkzNiwiZXhwIjoxNzQ4MzY2MzM2fQ.UP-2hPNrvLf8RiVug7kR8WIxGTkJJr432vtMYHTTNLg", process.env.JWT_SECRET);
     req.user = decoded; // Add user info to req object
+    console.log(req.user);
+    
     next(); // Proceed to next middleware or route handler
   } catch (error) {
+    console.log(error);
+    
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
